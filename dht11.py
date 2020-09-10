5a22c024f2991c89a4205971d02a5803e0075b0b
Panos Rontogiannis <p_ronto@hotmail.com>
2020-09-10 12:23:05 +0300

    fix metrics format and replacerelative with absolute paths

--- dht11.py
+++ dht11.py
@@ -10,6 +10,7 @@ pin = 4
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 if humidity is not None and temperature is not None:
-    print('dht_temperature={{ device="DHT11" }} {0:0.1f} dht_humidity{{ device="DHT11" }} {1:0.1f}'.format(temperature, humidity))
+    print('dht_temperature{{ device="DHT11" }} {:0.1f}'.format(temperature))
+    print('dht_humidity{{ device="DHT11" }} {:0.1f}'.format(humidity))
 else:
     print('')
